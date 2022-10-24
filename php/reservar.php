1426b960421e3ebaa419edcf2d64faace128e652
NandoBravoAgustin <112024459+NandoBravoAgustin@users.noreply.github.com>
2022-10-24 15:14:54 +0200

    update

--- php/reservar.php
+++ php/reservar.php
@@ -6,8 +6,9 @@ $num_personas = $_POST["personas_num"];
 $numero_reserva = $_POST["numreserva_hid"];
 $fechas_reservadas = array();
 
+// Fechas a reservar
 include_once 'conexion2.php';
-$consulta = "INSERT INTO reservation (habitacion, fecha_entrada, fecha_salida, cantidad_personas,id) VALUES ('$room_actual','$fecha_ida','$fecha_vuelta','$num_personas','')";
+$consulta = "INSERT INTO reservation (habitacion, fecha_entrada, fecha_salida, cantidad_personas,id) VALUES ('$room_actual','$fecha_ida','$fecha_vuelta','$num_personas','1')";
 $ejecutar_consulta = $conectar->query(utf8_encode($consulta));
 $conectar->close();
 header("Location: https://hotelmarkanando.herokuapp.com/php/detallehabitacion.php?reserva=exitosa&modulo=detallehabitacion&room=$room_actual&mensaje=reserva_exitosa");
@@ -16,3 +17,6 @@ header("Location: https://hotelmarkanando.herokuapp.com/php/detallehabitacion.ph
 
 
  ?>
+ <script>
+ 	console.log("prueba")
+ </script>
