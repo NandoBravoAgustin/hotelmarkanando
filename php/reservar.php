ed9a39fc121653528dba1f5eb783e329d52d8662
NandoBravoAgustin <112024459+NandoBravoAgustin@users.noreply.github.com>
2022-10-24 15:08:22 +0200

    update

--- php/reservar.php
+++ php/reservar.php
@@ -1,4 +1,3 @@
-
 <?php 
 $room_actual = $_POST["room_hid"];
 $fecha_ida = $_POST["fechaida_dat"];
@@ -7,12 +6,53 @@ $num_personas = $_POST["personas_num"];
 $numero_reserva = $_POST["numreserva_hid"];
 $fechas_reservadas = array();
 
-// Fechas a reservar
+
+
+
 $intervalo_fechas = array();
 for ($i=$fecha_ida; $i <= $fecha_vuelta; $i = date("Y-m-d", strtotime($i."+ 1 days"))) { 
 	array_push($intervalo_fechas, $i);
 };
 
-echo "room actual : ".$room_actual."<br>"."fecha ida : ".$fecha_ida."<br>"."fecha vuelta : ".$fecha_vuelta."<br>"."num personas : ".$num_personas."<br>"."num reserva : ".$numero_reserva."<br>"."fechas reservadas : <br>".$fechas_reservadas;
+
+
+include_once 'conexion.php';
+$consulta = "SELECT fecha_entrada,fecha_salida FROM reservation WHERE habitacion = '$room_actual'";
+$ejecutar_consulta = $conexion->query($consulta);
+while ($registro = $ejecutar_consulta->fetch_assoc()) {
+	$fecha_1 = $registro["fecha_entrada"];
+	$fecha_2 = $registro["fecha_salida"];
+
+	for ($i=$fecha_1; $i <= $fecha_2; $i = date("Y-m-d", strtotime($i."+ 1 days"))) { 
+		array_push($fechas_reservadas, $i);	
+	};
+};
+
+
+$consulta_persona = "SELECT person FROM rooms WHERE nombre = '$room_actual'";
+$ejecutar_consulta_persona = $conexion->query($consulta_persona);
+$registro_personas = $ejecutar_consulta_persona->fetch_assoc();
+$max_personas = $registro_personas["person"];
+$conexion->close();
+// reservar
+if (array_intersect($intervalo_fechas, $fechas_reservadas) ) {
+	echo "la fecha <b>($fecha_ida entre $fecha_vuelta)</b> no esta disponible por que esta reservada MAKINOLA<br>";
+
+	header("Location: https://hotelmarkanando.herokuapp.com/php/detallehabitacion.php?modulo=detallehabitacion&room=$room_actual&mensaje=reserva_fallida");
+
+} else if ($max_personas >= $num_personas){
+	include_once 'conexion2.php';
+		$consulta = "INSERT INTO reservation (habitacion, fecha_entrada, fecha_salida, cantidad_personas,id) VALUES ('$room_actual','$fecha_ida','$fecha_vuelta','$num_personas','')";
+		$ejecutar_consulta = $conectar->query(utf8_encode($consulta));
+		$conectar->close();
+		header("Location: https://hotelmarkanando.herokuapp.com/php/detallehabitacion.php?modulo=detallehabitacion&room=$room_actual&mensaje=reserva_exitosa");
+
+} else {
+	header("Location: https://hotelmarkanando.herokuapp.com/php/detallehabitacion.php?modulo=detallehabitacion&room=$room_actual&mensaje=reserva_fallida_persona");
+}
+;
+
+	
+
 
  ?>
