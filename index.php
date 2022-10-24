22da1e476bf1213f31edcf0902f4395bf660f456
NandoBravoAgustin <112024459+NandoBravoAgustin@users.noreply.github.com>
2022-10-24 14:43:57 +0200

    update index

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ if ($modulo == "detallehabitacion") {
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Markanando Hotel</title>
+	<title>Prueba 1</title>
 	<link rel="stylesheet" href="styles.css">
 	
 </head>
