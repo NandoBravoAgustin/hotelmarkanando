c1ea254148a557b17566d36c2f5a4c6ca0327b85
NandoBravoAgustin <112024459+NandoBravoAgustin@users.noreply.github.com>
2022-10-24 14:50:09 +0200

    update

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ if ($modulo == "detallehabitacion") {
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Prueba 1</title>
+	<title>Prueba 2</title>
 	<link rel="stylesheet" href="styles.css">
 	
 </head>
