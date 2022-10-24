33f63c2eab4333a4072c075e3fd27ee941252799
NandoBravoAgustin <112024459+NandoBravoAgustin@users.noreply.github.com>
2022-10-24 15:05:26 +0200

    update

--- php/reservar.php
+++ php/reservar.php
@@ -1,8 +1,18 @@
+
 <?php 
+$room_actual = $_POST["room_hid"];
+$fecha_ida = $_POST["fechaida_dat"];
+$fecha_vuelta = $_POST["fechavuelta_dat"];
+$num_personas = $_POST["personas_num"];
+$numero_reserva = $_POST["numreserva_hid"];
+$fechas_reservadas = array();
 
+// Fechas a reservar
+$intervalo_fechas = array();
+for ($i=$fecha_ida; $i <= $fecha_vuelta; $i = date("Y-m-d", strtotime($i."+ 1 days"))) { 
+	array_push($intervalo_fechas, $i);
+};
 
-
-header("Location: https://hotelmarkanando.herokuapp.com/php/detallehabitacion.php?modulo=detallehabitacion&room=$room_actual&op=funciono");
-
+echo "room actual : ".$room_actual."<br>"."fecha ida : ".$fecha_ida."<br>"."fecha vuelta : ".$fecha_vuelta."<br>"."num personas : ".$num_personas."<br>"."num reserva : ".$numero_reserva."<br>"."fechas reservadas : <br>".$fechas_reservadas;
 
  ?>
