eca169dbc7fcbe0adc2079ffeb3d5d5ad1830177
NandoBravoAgustin <112024459+NandoBravoAgustin@users.noreply.github.com>
2022-10-27 03:50:54 +0200

    add files

---
+++ php/reserva_exitosa.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" href="../styles.css">
+</head>
+<body>
+	<section id="mensaje_exitoso">
+		<div>
+			<h1>Reserva Exitosa</h1>
+			<h3>Se reservo exitosamente la habitacion</h3>
+		
+			<?php 
+				$room_actual = $_GET["room"];
+				$fecha_ida = $_GET["fecha_ida"];
+				$fecha_vuelta = $_GET["fecha_vuelta"];
+				$num_personas = $_GET["num_personas"];
+				$numero_reserva = $_GET["numero_reserva"];
+				$price = $_GET["price"];
+
+
+
+
+			echo "<a href='../ticket/ticket.php?room=$room_actual&fecha_ida=$fecha_ida&fecha_vuelta=$fecha_vuelta&num_personas=$num_personas&numero_reserva=$numero_reserva&price=$price' download='doc.pdf'>Descargar Comprobante</a>"; ?>
+			
+		</div>			
+	</section>			
+</body>
+</html>
+
+
+
