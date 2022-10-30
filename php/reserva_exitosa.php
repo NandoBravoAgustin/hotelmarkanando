b4e2c9158f4f2f685d8c6d6ad15f256f08092c32
NandoBravoAgustin <112024459+NandoBravoAgustin@users.noreply.github.com>
2022-10-30 18:44:09 +0100

    add files

---
+++ php/reserva_exitosa.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" href="../styles.css">
+</head>
+<body>
+	<section id="mensaje_exitoso">
+		<div class="mensaje">
+			<h1>Reserva Exitosa</h1>
+			<h3>Se reservo exitosamente la habitacion</h3>
+		
+			<?php 
+				$room_actual = $_GET["room"];
+				$fecha_ida = $_GET["fecha_ida"];
+				$fecha_vuelta = $_GET["fecha_vuelta"];
+				$num_personas = $_GET["num_personas"];
+				$price = $_GET["price"];
+				$id_reserva = $_GET["id"];
+				$dni = $_GET["dni"];
+				$cliente = $_GET["cliente"];
+				$dias = $_GET["dias"];
+				$total = $_GET["total"];
+
+
+
+
+			echo "<a href='../ticket/ticket.php?id=$id_reserva&room=$room_actual&fecha_ida=$fecha_ida&fecha_vuelta=$fecha_vuelta&num_personas=$num_personas&price=$price&dni=$dni&cliente=$cliente&total=$total&dias=$dias' download='doc.pdf'>Descargar Comprobante</a>"; 
+			echo "<a href='detallehabitacion.php?modulo=detallehabitacion&room=$room_actual&cliente=$cliente'>Volver</a>";
+			?>
+
+			
+		</div>			
+	</section>			
+</body>
+</html>
+
+
+
---
+++ php/reserva_fallida_personas.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" href="../styles.css">
+</head>
+<body>
+	<section id="mensaje_fallido">
+		<div class="mensaje">
+			<h1>Reserva Fallida</h1>
+			<h3>No se pudo reservar esta habitacion por que la cantidad de personas ingresadas supera la cantidad permitida</h3>
+		
+			<?php 
+				$room_actual = $_GET["room"];
+				
+
+
+
+			 
+			echo "<div><a href='detallehabitacion.php?modulo=detallehabitacion&room=$room_actual'>Volver</a></div>";
+			?>
+
+			
+		</div>			
+	</section>			
+</body>
+</html>
+
+
+
